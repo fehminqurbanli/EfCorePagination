c9c2957cc78f36db50aae1c6d3ce10e43238c97e
Fahmin <fehminqurbanli2@gmail.com>
2022-09-19 13:32:11 +0400

    adding ef core include methods

---
+++ EfCoreTutorialDotNet6/Include/Models/Team.cs
@@ -0,0 +1,10 @@
+﻿namespace EfCoreTutorialDotNet6.Include.Models
+{
+    public class Team
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }=String.Empty;
+        public int ComicId { get; set; }
+        public List<SuperHero> SuperHeroes { get; set; }=new List<SuperHero>();
+    }
+}
